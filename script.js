efa3be898bf70ee79c14a6fe6631b15a2c225ce3
judetime <maja.sobierajska@gmail.com>
2019-03-15 08:52:49 +0100

    sleep funtion doesn't work

--- script.js
+++ script.js
@@ -3,5 +3,14 @@ fetch ("http://api.open-notify.org/iss-now.json")
 .then(response => response.json())
 .then(json => [json.iss_position.latitude, json.iss_position.longitude])
 .then(geo1 => console.log(geo1));
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+  }
+sleep(5000)
+.then (fetch ("http://api.open-notify.org/iss-now.json")
+.then(response => response.json())
+.then(json => [json.iss_position.latitude, json.iss_position.longitude])
+.then(geo1 => console.log(geo1)));
+
 
 //fetch - > 5s sleep -> fetch
